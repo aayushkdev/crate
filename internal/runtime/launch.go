3cbdfa7894f31f430806e7116aa59ddfb9839a4d
Aayush Kumar <code@aayushk.dev>
2025-12-29 02:03:56 +0530

    add namespace and pid isolation

--- internal/runtime/launch.go
+++ internal/runtime/launch.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func LaunchContainer(args []string) error {
@@ -11,6 +12,12 @@ func LaunchContainer(args []string) error {
 		"/proc/self/exe",
 		append([]string{"init"}, args...)...,
 	)
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
